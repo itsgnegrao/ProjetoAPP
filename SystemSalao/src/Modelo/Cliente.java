aeb65e9272fac7dd6794d4282928a93699a8f945
Quejao <Leotrone@hotmail.com>
2016-05-17 00:17:48 -0300

    arrumando classe Cliente

--- SystemSalao/src/Modelo/Cliente.java
+++ SystemSalao/src/Modelo/Cliente.java
@@ -7,7 +7,9 @@ package Modelo;
 
 import Controle.ConnectionFactory;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -18,14 +20,24 @@ public class Cliente {
 
     int id_cli;
     String nome_cli;
-    String data_nasc_cli;
+    Date data_nasc_cli;
     String cpf_cli;
     int login_id;
     String celular;
+    
     public Cliente(String string) throws SQLException {
         Connection conexao = ConnectionFactory.createConnection();
         PreparedStatement ps;
         ps = conexao.prepareStatement(string);
         ps.execute();
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            this.id_cli = rs.getInt("id_cli");
+            this.nome_cli = rs.getString("nome_cli");
+            this.data_nasc_cli = rs.getDate("data_nasc_cli");
+            this.cpf_cli = rs.getString("cpf_cli");
+            this.login_id = rs.getInt("login_id");
+            this.celular = rs.getString("celular");
+        }
     }
 }
