d4de9f1c2f68d91e42a669f08cb268dc0447460a
GIOVANI HENRIQUE BERTUZZO <giovani.hbertuzzo@gmail.com>
2016-05-23 20:47:43 -0300

    reformulado layout, excluido todos panels, e criado frames no lugar

--- SystemSalao/src/Visao/FramePrincipal.java
+++ SystemSalao/src/Visao/FramePrincipal.java
@@ -13,14 +13,8 @@ import javax.swing.JPanel;
  *
  * @author leonardo corrá, giovani bertuzzo, gabriel negrão
  */
-public class FramePrincipal extends javax.swing.JFrame {
-    
-        JPanel panelCadCli = new PanelCadCli();
-        JPanel panelCadFunc = new PanelCadFunc();
-        JPanel panelAgenda = new PanelAgenda();
-        JPanel panelAgendaDois = new PanelAgendaDois();
-        JPanel panelCaixa = new PanelCaixa();
-        Container c=getContentPane();
+public class FramePrincipal extends javax.swing.JFrame {    
+        
     /**
      * Creates new form NovoJFrame
      */
@@ -48,12 +42,12 @@ public class FramePrincipal extends javax.swing.JFrame {
         jPanelPrinc = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuCadCli = new javax.swing.JMenuItem();
+        jMenuCadFunc = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
-        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuAgenda = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
-        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenuCaixa = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -61,15 +55,35 @@ public class FramePrincipal extends javax.swing.JFrame {
 
         jButton2.setText("cc");
         jButton2.setPreferredSize(new java.awt.Dimension(50, 50));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton1.setText("cf");
         jButton1.setPreferredSize(new java.awt.Dimension(50, 50));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("ag");
         jButton4.setPreferredSize(new java.awt.Dimension(50, 50));
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("cx");
         jButton3.setPreferredSize(new java.awt.Dimension(50, 50));
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -104,8 +118,6 @@ public class FramePrincipal extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jPanelPrinc.setBackground(java.awt.Color.white);
-
         javax.swing.GroupLayout jPanelPrincLayout = new javax.swing.GroupLayout(jPanelPrinc);
         jPanelPrinc.setLayout(jPanelPrincLayout);
         jPanelPrincLayout.setHorizontalGroup(
@@ -119,45 +131,45 @@ public class FramePrincipal extends javax.swing.JFrame {
 
         jMenu1.setText("Cadastros");
 
-        jMenuItem2.setText("Cliente");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+        jMenuCadCli.setText("Cliente");
+        jMenuCadCli.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
+                jMenuCadCliActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem2);
+        jMenu1.add(jMenuCadCli);
 
-        jMenuItem1.setText("Funcionário");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+        jMenuCadFunc.setText("Funcionário");
+        jMenuCadFunc.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
+                jMenuCadFuncActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem1);
+        jMenu1.add(jMenuCadFunc);
 
         jMenuBar1.add(jMenu1);
 
         jMenu3.setText("Agenda");
 
-        jMenuItem4.setText("Consultar Agenda");
-        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+        jMenuAgenda.setText("Consultar Agenda");
+        jMenuAgenda.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem4ActionPerformed(evt);
+                jMenuAgendaActionPerformed(evt);
             }
         });
-        jMenu3.add(jMenuItem4);
+        jMenu3.add(jMenuAgenda);
 
         jMenuBar1.add(jMenu3);
 
         jMenu4.setText("Caixa");
 
-        jMenuItem7.setText("Consultar Caixa");
-        jMenuItem7.addActionListener(new java.awt.event.ActionListener() {
+        jMenuCaixa.setText("Consultar Caixa");
+        jMenuCaixa.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem7ActionPerformed(evt);
+                jMenuCaixaActionPerformed(evt);
             }
         });
-        jMenu4.add(jMenuItem7);
+        jMenu4.add(jMenuCaixa);
 
         jMenuBar1.add(jMenu4);
 
@@ -187,80 +199,53 @@ public class FramePrincipal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-        c.removeAll();
-        c.repaint();
-        panelCadFunc.setBounds(5,5,500,500);
-        //panelCadFunc.setBackground(Color.BLUE);
-        // obter dimensões do pai  
-        int larguraPai   = FramePrincipal.this.getWidth();  
-        int alturaPai    = FramePrincipal.this.getHeight();  
-        // obter dimensões do filho  
-        int larguraFilho = panelCadFunc.getWidth();  
-        int alturaFilho  = panelCadFunc.getWidth();  
-        // calcular novas coordenadas do filho  
-        int novoX        = (larguraPai - larguraFilho) / 2;  
-        int novoY        = (alturaPai - alturaFilho) / 2;  
-        // centralizar filho  
-        panelCadFunc.setLocation(novoX, novoY);
-        panelCadFunc.setVisible(true);
-        c.add(panelCadFunc);       // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
+    private void jMenuCadFuncActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuCadFuncActionPerformed
+       FrameCadFunc fcc1 = new FrameCadFunc();
+       fcc1.setLocation(5,5);
+       fcc1.setVisible(true);
+    }//GEN-LAST:event_jMenuCadFuncActionPerformed
 
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        c.removeAll();
-        //jPanelPrinc.add(panelCadCli);
-        panelAgendaDois.setBounds(5,290,330,235);
-        //panelCadCli.setLocation(0, 0);
-        panelCadCli.setVisible(true);
-                // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
+    private void jMenuCadCliActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuCadCliActionPerformed
+       FrameCadCli fcc1 = new FrameCadCli();
+       fcc1.setLocation(100,100);
+       fcc1.setVisible(true);
+    }//GEN-LAST:event_jMenuCadCliActionPerformed
 
-    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
-        c.removeAll();
-        c.repaint();
-        panelAgenda.setBounds(5,5,600,600);
-        //panelAgenda.setBackground(Color.GREEN);
-        // obter dimensões do pai  
-        int larguraPai   = FramePrincipal.this.getWidth();  
-        int alturaPai    = FramePrincipal.this.getHeight();  
-        // obter dimensões do filho  
-        int larguraFilho = panelAgenda.getWidth();  
-        int alturaFilho  = panelAgenda.getWidth();  
-        // calcular novas coordenadas do filho  
-        int novoX        = (larguraPai - larguraFilho) / 2;  
-        int novoY        = (alturaPai - alturaFilho) / 2;  
-        // centralizar filho  
-        panelAgenda.setLocation(novoX, novoY);
-        panelAgenda.setVisible(true);        
-        
-        //        
-        panelAgenda.add(panelAgendaDois);
-        panelAgendaDois.setLocation(novoX, novoY);
-        panelAgendaDois.setVisible(true);
-        panelAgendaDois.setBounds(5,290,330,235);
-        c.add(panelAgenda);
-    }//GEN-LAST:event_jMenuItem4ActionPerformed
+    private void jMenuAgendaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuAgendaActionPerformed
+       FrameAgenda fa1 = new FrameAgenda();
+       fa1.setLocation(100,100);
+       fa1.setVisible(true);
+    }//GEN-LAST:event_jMenuAgendaActionPerformed
 
-    private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem7ActionPerformed
-        c.removeAll();
-        c.repaint();
-        panelCaixa.setBounds(5,5,500,500);
-        // obter dimensões do pai  
-        int larguraPai   = FramePrincipal.this.getWidth();  
-        int alturaPai    = FramePrincipal.this.getHeight();  
-        // obter dimensões do filho  
-        int larguraFilho = panelCaixa.getWidth();  
-        int alturaFilho  = panelCaixa.getWidth();  
-        // calcular novas coordenadas do filho  
-        int novoX        = (larguraPai - larguraFilho) / 2;  
-        int novoY        = (alturaPai - alturaFilho) / 2;  
-        // centralizar filho  
-        panelCaixa.setLocation(novoX, novoY);
-        panelCaixa.setBackground(Color.WHITE);
-        panelCaixa.setVisible(true);
-        c.add(panelCaixa);
-    }//GEN-LAST:event_jMenuItem7ActionPerformed
+    private void jMenuCaixaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuCaixaActionPerformed
+       FrameCaixa fc1 = new FrameCaixa();
+       fc1.setLocation(100,100);
+       fc1.setVisible(true);
+    }//GEN-LAST:event_jMenuCaixaActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        FrameCadCli fcc1 = new FrameCadCli();
+       fcc1.setLocation(100,100);
+       fcc1.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        FrameCadFunc fcc1 = new FrameCadFunc();
+       fcc1.setLocation(5,5);
+       fcc1.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        FrameAgenda fa1 = new FrameAgenda();
+       fa1.setLocation(100,100);
+       fa1.setVisible(true);
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        FrameCaixa fc1 = new FrameCaixa();
+       fc1.setLocation(100,100);
+       fc1.setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -275,11 +260,11 @@ public class FramePrincipal extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenuItem jMenuAgenda;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuCadCli;
+    private javax.swing.JMenuItem jMenuCadFunc;
+    private javax.swing.JMenuItem jMenuCaixa;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanelPrinc;
     // End of variables declaration//GEN-END:variables
