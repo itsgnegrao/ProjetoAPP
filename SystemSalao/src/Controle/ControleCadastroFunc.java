9440e053d1be9ecc7bd6c8a9cb4acac8048cf917
Quejao <Leotrone@hotmail.com>
2016-06-02 15:32:30 -0300

    Ajustados controle de Funcionario

--- SystemSalao/src/Controle/ControleCadastroFunc.java
+++ SystemSalao/src/Controle/ControleCadastroFunc.java
@@ -7,6 +7,7 @@ package Controle;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,16 +18,22 @@ import java.util.logging.Logger;
  */
 public class ControleCadastroFunc {
     int id_func;
-    int idade_func;
-    String nome_func;
-    String cpf_func;
-    String pis_func;
-    float salario_func;
-    int cargo_id;
-    int login_id;
-    public ControleCadastroFunc(String id_func, String idade_func, String nome_func, String cpf_func, String pis_func, String salario_func, String cargo_id, String login_id) throws SQLException {
+    public ControleCadastroFunc(String idade_func, String nome_func, String cpf_func, String pis_func, String salario_func, String cargo_id, String login_id) throws SQLException {
         try{
-            String string = "Insert INTO Cliente VALUES(" + id_func + "," + idade_func + ",'" + nome_func + "','" + cpf_func + "','" + pis_func + "'," + salario_func + "," + cargo_id + "," + login_id + ");";
+            String string1 = "Select id_func from Funcionario";
+            Connection conexao1 = ConnectionFactory.createConnection();
+            PreparedStatement ps1;
+            ps1 = conexao1.prepareStatement(string1);
+            ps1.execute();
+            ResultSet rs = ps1.executeQuery();
+            while(rs.next()){
+                id_func = rs.getInt("id_func");
+            }
+            conexao1.close();
+            
+            id_func += 2;
+            
+            String string = "Insert INTO Funcionario VALUES(" + id_func + "," + idade_func + ",'" + nome_func + "','" + cpf_func + "','" + pis_func + "'," + salario_func + "," + cargo_id + "," + login_id + ");";
             Connection conexao = ConnectionFactory.createConnection();
             PreparedStatement ps;
             ps = conexao.prepareStatement(string);
