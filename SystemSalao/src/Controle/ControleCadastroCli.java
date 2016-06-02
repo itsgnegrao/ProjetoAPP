c978a9c5c711552eb167007460f57785c8ccfc49
Quejao <Leotrone@hotmail.com>
2016-06-02 15:16:10 -0300

    Ajustados controle de Cliente

--- SystemSalao/src/Controle/ControleCadastroCli.java
+++ SystemSalao/src/Controle/ControleCadastroCli.java
@@ -19,15 +19,27 @@ import java.util.logging.Logger;
  * @author Leonardo
  */
 public class ControleCadastroCli {
-    
-    public ControleCadastroCli(String id_cli, String nome_cli, String data_nasc_cli, String cpf_cli, String login_id, String celular) throws SQLException {
+    int id_cli;
+    public ControleCadastroCli(String nome_cli, String data_nasc_cli, String cpf_cli, String login_id, String celular) throws SQLException {
         try{
-            String string = "Insert INTO Cliente VALUES(" + id_cli + ",'" + nome_cli + "'," + data_nasc_cli + ",'" + cpf_cli + "'," + login_id + ",'" + celular + "');";
+            String string = "Select id_cli from Cliente";
             Connection conexao = ConnectionFactory.createConnection();
             PreparedStatement ps;
             ps = conexao.prepareStatement(string);
             ps.execute();
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                id_cli = rs.getInt("id_cli");
+            }
             conexao.close();
+            
+            id_cli += 2;
+            String string1 = "Insert INTO Cliente VALUES(" + id_cli + ",'" + nome_cli + "'," + data_nasc_cli + ",'" + cpf_cli + "'," + login_id + ",'" + celular + "');";
+            Connection conexao1 = ConnectionFactory.createConnection();
+            PreparedStatement ps1;
+            ps1 = conexao1.prepareStatement(string1);
+            ps1.execute();
+            conexao1.close();
         } catch(SQLException ex) {
             Logger.getLogger(ControleCadastroCli.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("DEU NAO MLEKOTE");
