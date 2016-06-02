bac2a0b040cb0696b46f60fb221846ed043844cf
Quejao <Leotrone@hotmail.com>
2016-06-02 14:39:11 -0300

    Ajustado modelo de funcionario

--- SystemSalao/src/Modelo/Funcionario.java
+++ SystemSalao/src/Modelo/Funcionario.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 
 /**
  *
- * @author Negrão
+ * @author NegrÃ£o
  */
 public class Funcionario {
 
@@ -22,22 +22,15 @@ public class Funcionario {
     int cargo_id;
     int login_id;
 
-    public Funcionario(String string) throws SQLException {
-        Connection conexao = ConnectionFactory.createConnection();
-        PreparedStatement ps;
-        ps = conexao.prepareStatement(string);
-        ps.execute();
-        ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
-            this.id_func = rs.getInt("id_func");
-            this.idade_func = rs.getInt("idade_func");
-            this.nome_func = rs.getString("nome_func");
-            this.cpf_func = rs.getString("cpf_func");
-            this.pis_func = rs.getString("pis_func");
-            this.salario_func = rs.getFloat("salario_func");
-            this.cargo_id = rs.getInt("cargo_id");
-            this.login_id = rs.getInt("login_id");
-        }
-        conexao.close();
+    public Funcionario(int id_func, int idade_func, String nome_func, String cpf_func, String pis_func, float salario_func, int cargo_id, int login_id) {
+        this.id_func = id_func;
+        this.idade_func = idade_func;
+        this.nome_func = nome_func;
+        this.cpf_func = cpf_func;
+        this.pis_func = pis_func;
+        this.salario_func = salario_func;
+        this.cargo_id = cargo_id;
+        this.login_id = login_id;
     }
+    
 }
