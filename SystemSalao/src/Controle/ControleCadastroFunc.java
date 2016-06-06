872203a8a2b903d6b2ed4f895f79a1df4e948e15
Quejao <Leotrone@hotmail.com>
2016-06-06 18:31:56 -0300

    Ajustado controle de cadastro para conexão com a tela

--- SystemSalao/src/Controle/ControleCadastroFunc.java
+++ SystemSalao/src/Controle/ControleCadastroFunc.java
@@ -5,6 +5,9 @@
  */
 package Controle;
 
+import Modelo.Cliente;
+import Modelo.Funcionario;
+import Modelo.Login;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,6 +20,34 @@ import java.util.logging.Logger;
  * @author Leonardo
  */
 public class ControleCadastroFunc {
+    Funcionario funcionario;
+    Login funcionario_login;
+    
+    public ControleCadastroFunc(int id_func) {
+        try {
+            String string = "Select * from Funcionario where id_func=" + id_func + ";";
+            String string1 = "Select * from Login natural join funcionario_login";
+            Connection conexao = ConnectionFactory.createConnection();
+            PreparedStatement ps;
+            ps = conexao.prepareStatement(string);
+            ps.execute();
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                funcionario = new Funcionario( rs.getInt("id_func"), rs.getInt("idade_func"), rs.getString("nome_func") , rs.getString("cpf_func"), rs.getString("pis_func"), rs.getFloat("salario_func"), rs.getInt("cargo_id"), rs.getInt("login_id"));
+            }
+
+            ps = conexao.prepareStatement(string1);
+            ps.execute();
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                funcionario_login = new Login(id_func, rs.getString("user"), rs.getString("senha"));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ControleCadastroCli.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     int id_func;
     public ControleCadastroFunc(String idade_func, String nome_func, String cpf_func, String pis_func, String salario_func, String cargo_id, String login_id) throws SQLException {
         try{
