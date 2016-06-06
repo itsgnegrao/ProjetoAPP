52027a1001af73543a2d94e7a05724f656c0804f
Quejao <Leotrone@hotmail.com>
2016-06-06 18:31:30 -0300

    .

--- SystemSalao/src/Controle/ConnectionFactory.java
+++ SystemSalao/src/Controle/ConnectionFactory.java
@@ -1,8 +1,14 @@
 package Controle;
 
+import Modelo.Cliente;
+import Modelo.Login;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *Esta classe estabelece a conexao com o banco de dados e retorna o objeto conexao.
