3c9e13e8c203753c6ddbe991b581047ff47ce247
Quejao <Leotrone@hotmail.com>
2016-06-23 15:24:47 -0300

    Alterado ControleServico para cadastro e busca de serviços

--- SystemSalao/src/Controle/ControleServico.java
+++ SystemSalao/src/Controle/ControleServico.java
@@ -16,6 +16,8 @@ import java.util.logging.Logger;
 
 public class ControleServico {
 
+    int id_serv;
+    Servico servico;
     ArrayList<Servico> servList = new ArrayList<>();
 
     public ControleServico() {
@@ -59,4 +61,53 @@ public class ControleServico {
         return servList;
     }
 
+    public ControleServico(String descricao, String valor) throws SQLException {
+        try {
+            String string = "Select id_serv from Servico";
+            Connection conexao = ConnectionFactory.createConnection();
+            PreparedStatement ps;
+            ps = conexao.prepareStatement(string);
+            ps.execute();
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                id_serv = rs.getInt("id_serv");
+            }
+            conexao.close();
+
+            id_serv += 1;
+
+            String string1 = "Insert INTO Servico VALUES(" + id_serv + ",'" + descricao + "'," + valor + ");";
+            System.out.println(string1);
+            Connection conexao1 = ConnectionFactory.createConnection();
+            PreparedStatement ps1;
+            ps1 = conexao1.prepareStatement(string1);
+            ps1.execute();
+            conexao1.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ControleCadastroFunc.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("DEU NAO MLEKOTE");
+        }
+
+    }
+
+    public ControleServico(int id_serv) throws SQLException {
+        try {
+            String string = "Select * from Servico where id_serv=" + id_serv + ";";
+            Connection conexao = ConnectionFactory.createConnection();
+            PreparedStatement ps;
+            ps = conexao.prepareStatement(string);
+            ps.execute();
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                servico = new Servico(rs.getInt("id_serv"), rs.getString("descricao"), rs.getFloat("valor"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ControleCadastroFunc.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("DEU NAO MLEKOTE");
+        }
+    }
+
+    public Servico getServico() {
+        return servico;
+    }
 }
